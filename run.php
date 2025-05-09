cb0a0b4c855518d4d516e4c8f5bb28c2c55a6c17
Alex <aleshaascet@gmail.com>
2025-05-09 19:30:35 +0300

    add function return group id

--- run.php
+++ run.php
@@ -1,9 +1,6 @@
 <?php
-
-
 use BotVisa\Bot;
 
 include ('vendor/autoload.php');
-
 $bot=new Bot('test@gmail.com','CwGY2!sjc+.>tLu');
 $bot->start();
--- src/Bot.php
+++ src/Bot.php
@@ -2,33 +2,53 @@
 namespace BotVisa;
 class Bot
 {
-    const     DIR_COOKIE='src/logs/';
-    public    $FILE_COOKIE_USER;
-    public    $url;
+
+    /**
+     *
+     */
+    const          DIR_COOKIE='src/logs/';
+    public         $FILE_COOKIE_USER;
+    public         $url;
     public  static $post_field_auth=[];
+
+    /**
+     * @param $email
+     * @param $password
+     */
     public function __construct($email, $password)
     {
         self::$post_field_auth= [
-            'utf8' => '✓',
-            'user[email]' => $email,
-            'user[password]' => $password,
-            'policy_confirmed' => 1,
-            'commit' => 'Sign In'
+            'utf8'              => '✓',
+            'user[email]'       => $email,
+            'user[password]'    => $password,
+            'policy_confirmed'  => 1,
+            'commit'            => 'Sign In'
         ];
        $this->FILE_COOKIE_USER=self::DIR_COOKIE.'cookie_'.$email;
        $this->createCookieFile();
     }
+
+    /**
+     * @return void
+     */
     public function start():void
     {
         $this->openSignIn();
     }
 
+    /**
+     * @return void
+     */
     public  function createCookieFile():void
     {
        if(!file_exists($this->FILE_COOKIE_USER)){
            fopen($this->FILE_COOKIE_USER,'w');
        }
     }
+
+    /**
+     * @return void
+     */
     public function openSignIn():void
     {
         $this->url='https://ais.usvisa-info.com/en-ca/niv/users/sign_in';
@@ -39,14 +59,18 @@ class Bot
             CURLOPT_COOKIEFILE     =>$this->FILE_COOKIE_USER,
             CURLOPT_COOKIEJAR      =>$this->FILE_COOKIE_USER,
             CURLOPT_HTTPHEADER     => [
-                'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
-            ],
+                                       'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
+                                       ],
         ];
         $html=$this->fileGetContents($curl_options_array);
         $csrfToken=self::getCSRFToken($html);
         $this->auth($csrfToken);
     }
 
+    /**
+     * @param $csrfToken
+     * @return void
+     */
     public  function auth($csrfToken):void
     {
         $this->url='https://ais.usvisa-info.com/en-ca/niv/users/sign_in';
@@ -56,56 +80,59 @@ class Bot
             CURLOPT_HEADER         =>true,
             CURLOPT_POST           => true,
             CURLOPT_HTTPHEADER     => [
-                'X-CSRF-TOKEN: ' . $csrfToken,
-                'Content-Type: application/x-www-form-urlencoded',
-                'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
-            ],
+                                    'X-CSRF-TOKEN: ' . $csrfToken,
+                                    'Content-Type: application/x-www-form-urlencoded',
+                                    'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
+                                     ],
             CURLOPT_COOKIEFILE     =>$this->FILE_COOKIE_USER,
             CURLOPT_COOKIEJAR      =>$this->FILE_COOKIE_USER,
-            CURLOPT_POSTFIELDS     => self::returnQueryString(self::$post_field_auth)
+            CURLOPT_POSTFIELDS     =>self::returnQueryString(self::$post_field_auth)
         ];
-     $this->fileGetContents($curl_options_array);
-      $this->redirectAcount();
+      $this->fileGetContents($curl_options_array);
+      $this->acountPage();
     }
 
-    public  function redirectAcount():void
+    /**
+     * @return void
+     */
+    public  function acountPage():void
     {
         $this->url='https://ais.usvisa-info.com/en-ca/niv/account';
         $curl_options_array=[
             CURLOPT_URL            =>$this->url,
             CURLOPT_RETURNTRANSFER =>true,
             CURLOPT_POST           => false,
-      //      CURLOPT_HEADER         =>true,
+      //    CURLOPT_HEADER        =>true,
             CURLOPT_HTTPHEADER     => [
-                'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
-            ],
+                                        'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
+                                       ],
             CURLOPT_COOKIEFILE     =>$this->FILE_COOKIE_USER,
             CURLOPT_COOKIEJAR      =>$this->FILE_COOKIE_USER,
 
         ];
 
-      $html=$this->fileGetContents($curl_options_array);
-        if (preg_match('/\/groups\/(\d+)/', $html, $matches)) {
-            $groupId = $matches[1];
-            echo "Номер группы: " . $groupId;
-        } else {
-            echo "Номер группы не найден!";
-        }
 
-        $this->redirectGroup($groupId);
+       $html=$this->fileGetContents($curl_options_array);
+       $groupId=self::getGroupId($html);
+
+       $this->groupPage($groupId);
     }
 
-    public  function redirectGroup($groupId):void
+    /**
+     * @param $groupId
+     * @return void
+     */
+    public  function groupPage($groupId):void
     {
         $this->url='https://ais.usvisa-info.com/en-ca/niv/groups/'.$groupId;
         $curl_options_array=[
             CURLOPT_URL            =>$this->url,
             CURLOPT_RETURNTRANSFER =>true,
-            CURLOPT_POST           => false,
+            CURLOPT_POST           =>false,
             CURLOPT_HEADER         =>true,
             CURLOPT_HTTPHEADER     => [
-                'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
-            ],
+                                      'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
+                                      ],
             CURLOPT_COOKIEFILE     =>$this->FILE_COOKIE_USER,
             CURLOPT_COOKIEJAR      =>$this->FILE_COOKIE_USER,
 
@@ -113,6 +140,10 @@ class Bot
         echo $this->fileGetContents($curl_options_array);
     }
 
+    /**
+     * @param $headers
+     * @return string
+     */
 
     public function fileGetContents($headers) :string
     {
@@ -123,6 +154,10 @@ class Bot
         return $response;
     }
 
+    /**
+     * @param $html
+     * @return string
+     */
     static function getCSRFToken($html):string
     {
         $dom =new \DOMDocument();
@@ -141,7 +176,11 @@ class Bot
         return $csrfToken ;
     }
 
-    static  function returnQueryString($data):string
+    /**
+     * @param $data
+     * @return string
+     */
+    static  function getQueryString($data):string
     {
         $queryString = http_build_query($data);
 
@@ -152,7 +191,25 @@ class Bot
         );
         $queryString = str_replace('%21', '!', $queryString);
 
-      return  $queryString = str_replace('utf8=%3F', 'utf8=%E2%9C%93', $queryString);
+        return  $queryString = str_replace('utf8=%3F', 'utf8=%E2%9C%93', $queryString);
+    }
+
+    /**
+     * @param $html
+     * @return string
+     *
+     */
+    static  function getGroupId($html):string
+    {
+        $groupId='';
+        if (preg_match('/\/groups\/(\d+)/', $html, $matches))
+        {
+            $groupId = $matches[1];
+            echo "Номер группы: " . $groupId;
+        } else {
+            echo "Номер группы не найден!";
+        }
+        return $groupId;
     }
 
 }
